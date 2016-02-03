2d842084617b3ab6c3950e6af2c027813164593a
James Feigel <james.feigel@sq1.com>
2016-02-03 07:05:31 -0600

    Changed to synchronous execution
    - Remove generator functions since this will all execute synchronously
    - Check for `package.json` and create the default one if it doesn’t
    exist
    - Run `npm install` if `package.json` already exists
    - Added color and reformatted the output

--- app.js
+++ app.js
@@ -1,108 +1,124 @@
 "use strict";
 
-const inquirer = require('inquirer');
-const co       = require('co');
-const execSync = require('child_process').execSync;
+const inquirer = require("inquirer");
+const co       = require("co");
+const execSync = require("child_process").execSync;
+const fs       = require("fs");
+const chalk    = require("chalk");
 
-const seperator = '\n=============================\n';
+const seperatorLine = "==============================";
+const seperator = `\n${seperatorLine}\n`
+
+let packageJSONExists = null;
+
+console.log(`\n${seperatorLine}`);
+console.log(chalk.gray.bgCyan('           JS-PAGES           '));
+console.log(`${seperatorLine}\n`);
 
 inquirer.prompt([
 	{
-		"type": "list",
-		"name": "grunt_gulp",
-		"message": "Which task runner will you use?",
-		"choices": ["Grunt", "Gulp", "other"]
+	    "type": "list",
+	    "name": "grunt_gulp",
+	    "message": "Which task runner will you use?",
+	    "choices": ["Grunt", "Gulp", "other"]
 	},
 	{
-		"type": "list",
-		"name": "js_ng",
-		"message": "Which documentation generator will you use?",
-		"choices": ["JSDoc", "ngdoc"]
+	    "type": "list",
+	    "name": "js_ng",
+	    "message": "Which documentation generator will you use?",
+	    "choices": ["ngdoc", "JSDoc"]
 	},
 	{
-		"type": "list",
-		"name": "sudo",
-		"message": "Are your npm packages installed with 'sudo'?",
-		"choices": [
-			{
-				"name": "Yes",
-				"value": true,
-				"short": "Yes"
-			},
-			{
-				"name": "No",
-				"value": false,
-				"short": "No"
-			}
-		]
+	    "type": "list",
+	    "name": "sudo",
+	    "message": "Are your npm packages installed with 'sudo'?",
+	    "choices": [
+		    {
+		        "name": "No",
+		        "value": false,
+		        "short": "No"
+		    },
+		    {
+		        "name": "Yes",
+		        "value": true,
+		        "short": "Yes"
+		    }
+	    ]
 	}
-], function(answers) {
-	co(function * () {
-		console.log(seperator);
-	    console.log(`Setting up selected task runner: ${answers.grunt_gulp}`);
+], (answers) => {
+    console.log(seperator);
+    console.log(chalk.gray(`Setting up selected task runner: ${chalk.reset(answers.grunt_gulp)}`));
 
-	    switch (answers.grunt_gulp) {
-	    	case 'Grunt':
-	    		yield grunt(answers);
-	    		break;
-	    	case 'Gulp':
-	    		break;
-	    	default:
-	    		break;
-	    }
+    try {
+        fs.statSync(`./package.json`);
+        console.log(chalk.green('package.json exists'));
+        packageJSONExists = true;
+        console.log(chalk.gray('Running npm install...'));
+        execSync('npm install > /dev/null 2>&1');
+    } catch (err) {
+        packageJSONExists = false;
+        console.log(chalk.yellow('package.json does not exist'));
+        console.log(chalk.gray('Creating package.json with default options...'));
+        execSync('npm init --force');
+    }
 
-	    console.log(`Setting up selected documentation generator: ${answers.js_ng}`);
-	}).then(function(success) {
-		console.log('Success!', success);
-	}, function(err) {
-		console.error('Error!', err);
-	});
+    switch (answers.grunt_gulp) {
+    case 'Grunt':
+        grunt(answers);
+        break;
+    case 'Gulp':
+        break;
+    default:
+        break;
+    }
+
+    console.log(chalk.gray(`Setting up selected documentation generator: ${chalk.reset(answers.js_ng)}`));
 });
 
 function checkInstall(packageToInstall, isGlobal) {
-	let command = 'npm list';
+    let command = 'npm list';
 
-	if (isGlobal === true) {
-		command = `${command} -g`;
-	}
+    if (isGlobal === true) {
+        command = `${command} -g`;
+    }
 
-	command = `${command} --depth 1 ${packageToInstall} > /dev/null 2>&1`
+    command = `${command} --depth 0 ${packageToInstall} > /dev/null 2>&1`
 
-	try {
-		execSync(command);
-		console.log(`${packageToInstall} is installed.`);
-		return true;
-	} catch (err) {
-		console.log(`${packageToInstall} is not installed.`);
-		return false;
-	}
+    try {
+        execSync(command);
+        console.log(chalk.green(`${packageToInstall} is installed.`));
+        return true;
+    } catch (err) {
+        console.log(chalk.yellow(`${packageToInstall} is not installed.`));
+        return false;
+    }
 }
 
 function installPackage(packageToInstall, isGlobal, isSudo) {
-	let command = 'npm install';
+    let command = 'npm install';
 
-	if (isSudo === true) {
-		command = `sudo ${command}`;
-	}
+    if (isSudo === true) {
+        command = `sudo ${command}`;
+    }
 
-	if (isGlobal === true) {
-		command = `${command} -g`;
-	}
+    if (isGlobal === true) {
+        command = `${command} -g`;
+    }
 
-	command = `${command} ${packageToInstall}`;
+    if (packageToInstall !== null) {
+        command = `${command} ${packageToInstall}`;
+    }
 
-	execSync(command);
-	return true;
+    execSync(command);
+    return true;
 }
 
-function * grunt(answers) {
-	console.log("Verifying 'grunt-cli' is installed globally...");
-	let isGruntCLIInstalled = checkInstall('grunt-cli', true);
+function grunt(answers) {
+    console.log(chalk.gray("Verifying 'grunt-cli' is installed globally..."));
+    let isGruntCLIInstalled = checkInstall('grunt-cli', true);
 
-	if (isGruntCLIInstalled === false) {
-		console.log("Installing 'grunt-cli' globally...");
-		installPackage('grunt-cli', true, answers.sudo);
-	}
-
-	// let isGruntInstalled = yield checkInstall('grunt', false);
+    if (isGruntCLIInstalled === false) {
+        console.log(chalk.gray("Installing 'grunt-cli' globally..."));
+        installPackage('grunt-cli', true, answers.sudo);
+    }
 }
